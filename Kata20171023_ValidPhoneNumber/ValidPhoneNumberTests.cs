c41e77de9fcbaebc05d3905c417ad67531103f29
cash <cash@cashwu.com>
2017-10-23 23:09:02 +0800

    add real phone number test case

--- Kata20171023_ValidPhoneNumber/ValidPhoneNumberTests.cs
+++ Kata20171023_ValidPhoneNumber/ValidPhoneNumberTests.cs
@@ -6,8 +6,19 @@ namespace Kata20171023_ValidPhoneNumber
     public class ValidPhoneNumberTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_real_phone_number()
         {
+            var kata = new Kata();
+            var result = kata.ValidPhoneNumber("(123) 456-7890");
+            Assert.AreEqual(true, result);
+        }
+    }
+
+    public class Kata
+    {
+        public bool ValidPhoneNumber(string phoneNumber)
+        {
+            return true;
         }
     }
 }
