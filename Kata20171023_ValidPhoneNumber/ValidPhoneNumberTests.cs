c77e51ea4e9323e54dc832183e1a34951b5d9bb2
cash <cash@cashwu.com>
2017-10-23 23:48:11 +0800

    input not three number in brackets

--- Kata20171023_ValidPhoneNumber/ValidPhoneNumberTests.cs
+++ Kata20171023_ValidPhoneNumber/ValidPhoneNumberTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace Kata20171023_ValidPhoneNumber
 {
@@ -27,13 +28,22 @@ namespace Kata20171023_ValidPhoneNumber
             var result = kata.ValidPhoneNumber("(123 456-7890");
             Assert.IsFalse(result);
         }
+
+        [TestMethod]
+        public void input_not_three_number_in_brackets()
+        {
+            var result = kata.ValidPhoneNumber("(aaa) 456-7890");
+            Assert.IsFalse(result);
+        }
     }
 
     public class Kata
     {
         public bool ValidPhoneNumber(string phoneNumber)
         {
-            if (phoneNumber[0] != '(' || phoneNumber[4] != ')')
+            if (phoneNumber[0] != '('
+                || phoneNumber[4] != ')'
+                || !phoneNumber.Substring(1, 3).All(a => int.TryParse(a.ToString(), out int b)))
             {
                 return false;
             }
