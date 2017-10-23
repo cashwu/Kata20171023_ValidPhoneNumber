d134f39739e7d88609008c74ef98ecc0f2758a0a
cash <cash@cashwu.com>
2017-10-23 22:59:03 +0800

    init

--- Kata20171023_ValidPhoneNumber/ValidPhoneNumberTests.cs
+++ Kata20171023_ValidPhoneNumber/ValidPhoneNumberTests.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20171023_ValidPhoneNumber
 {
     [TestClass]
-    public class UnitTest1
+    public class ValidPhoneNumberTests
     {
         [TestMethod]
         public void TestMethod1()
